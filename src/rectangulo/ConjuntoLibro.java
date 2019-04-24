8914368a7962a7c3cd1a9b7c9292f67a3e6b7aa8
Estudiante <Estudiante@BE401S103C21ING.unal.loc>
2019-04-24 08:17:20 -0500

    ya sirve casi toda esta monda

--- src/rectangulo/ConjuntoLibro.java
+++ src/rectangulo/ConjuntoLibro.java
@@ -15,19 +15,56 @@ public class ConjuntoLibro {
        this.libros = new Libro [10];
    }
  public boolean nuevoLibro(Libro libro1){
-     int i=0;
-     while (this.libros[i]==null){
-         i++;
+    for(int i=0;i<libros.length;i++){
+     if (libros[i]==null){
+         libros[i]=libro1;
+         return true;
      }
-     if(i<10){
-     this.libros[i]=libro1;
-     return true;
-     }
-     else{
-     return false;
-     }
+     
  }
+    
+    return false;
+     }
  public boolean eliminar(String nombre,String titulo){
- 
+ for(int i=0;i<libros.length;i++){
+     if(libros[i].getAutor().getNombre()==nombre && libros[i].getTitulo()==titulo)
+     {
+         libros[i]=null;
+     }
  }
+ return true;
+ }
+ public boolean mejor(){
+     int s=0;
+     for(int i=0;i<libros.length;i++){
+         if(libros[i]!=null){
+         if(libros[i].getCalificacion()>libros[s].getCalificacion()){
+             s=i;
+         } 
+         }
+     }
+     System.out.println(libros[s].getCalificacion());
+     return true;
+ }
+  public boolean peor(){
+     int s=0;
+     for(int i=0;i<libros.length;i++){
+         if(libros[i]!=null){
+             
+         if(libros[i].getCalificacion()<libros[s].getCalificacion()){
+             s=i;
+         }
+         }
+     }
+      System.out.println(libros[s].getCalificacion());
+         return true;
+ }
+  public boolean lista(){
+      for(int i =0;i<libros.length;i++){
+          if(libros[i]!=null){
+          System.out.println(libros[i].getTitulo()+"  "+libros[i].getAutor().getNombre()+"   " + libros[i].getAutor().getApellido());
+      }
+      }
+      return true;
+  }
 }
--- src/rectangulo/Main.java
+++ src/rectangulo/Main.java
@@ -12,9 +12,16 @@ package rectangulo;
 public class Main {
 
     public static void main(String[] args) {
-        Autor autor= new Autor("s","s");
-        Libro libro= new Libro("titulo", autor, 300, 8);
-
+        Autor autor= new Autor("Ana Lisa","Melchotto");
+        Libro libro= new Libro("la mamba negra", autor, 300, 8);
+        Autor autor1= new Autor("Benito","Camelo");
+        Libro libro1= new Libro("El bananon", autor1, 500, 6);
+        ConjuntoLibro conjunto = new ConjuntoLibro();
+        conjunto.nuevoLibro(libro);
+        conjunto.nuevoLibro(libro1);
+        conjunto.lista();
+        conjunto.mejor();
+        conjunto.peor();
     }
     
 }
